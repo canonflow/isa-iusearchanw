e385ab670400f0889b87e450d3a669d61721c4b7
AnastasyaP <anastasyap41@gmail.com>
2024-04-27 21:38:09 +0700

    home(update)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <h1 class="text-center">Selamat Datang di RAWAT JALAN INAP</h1>
+        <img src="{{asset('images/foto_dokter.png')}}" alt="harusnya sih foto gengs kita yah">
         {{-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -18,35 +19,6 @@
                 </div>
             </div>
         </div> --}}
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">id</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
     </div>
 </div>
 @endsection
